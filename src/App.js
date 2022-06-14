8dbda39e6919bc637dbf081fd3511f732a6063f8
Denisse Maldonado <denissemaldonado@Denisses-MacBook-Air.local>
2022-06-13 22:17:53 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useEffect } from "react";
 
-function App() {
+export default function App() {
+  function getData () {
+    return fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`)
+  }
+  
+  useEffect(() => {
+    getData()
+      .then(response => response.json())
+      .then(response => console.log(response))
+  }, []) 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
-export default App;
